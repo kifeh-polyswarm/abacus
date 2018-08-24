b82d62d0988509274c33a0c529790377378cd73e
alex-magana <alex.magana@andela.com>
2018-08-24 22:32:44 +0300

    Add support for multiplication

--- src/lib/compute.py
+++ src/lib/compute.py
@@ -1,4 +1,18 @@
 class Compute:
-	def __init__(self, operator, operand):
-		self.operator = operator
-		self.operands = operands
+    def __init__(self, operator, operands):
+        self.operator = operator
+        self.operands = operands
+
+    def multiply(self):
+        if self.operands is None:
+            return
+        product = 1
+        for item in self.operands:
+            product *= item
+        print(product)
+
+    def subtract(self):
+        difference = 0
+        for item in self.operands:
+            difference -= item
+        print(difference)

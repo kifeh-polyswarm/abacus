92726366ab366d72b8b8f77deffd305d4d46023b
alex-magana <alex.magana@andela.com>
2018-10-14 14:37:35 +0300

    Add scaffold

---
+++ src/lib/compute.py
@@ -0,0 +1,18 @@
+class Compute:
+    def __init__(self, operator, operands):
+        self.operator = operator
+        self.operands = operands
+
+    def add(self):
+        pass
+
+    def subtract(self):
+        pass
+
+    def divide(self):
+        pass
+
+    def multiply(self):
+        pass
+
+

4e9de68fd2e43a7343daecf7fdc0a65a6e6e8b6b
alex-magana <alex.magana@andela.com>
2018-10-24 09:57:36 +0300

    Define perimeter formulas

--- src/lib/util/util.py
+++ src/lib/util/util.py
@@ -1,10 +1,22 @@
+import math
+
 class Util(object):
     def __init__(self):
         self
     
     @classmethod
-    def perimeter(shape, *args):
-        pass
+    def perimeter(cls, shape, *args):
+        perimeter = 0
+        if shape == "square":
+            perimeter = args[0] * 4
+        elif shape == "rectangle":
+            perimeter = reduce(lambda x, y: (2 * x) + (2 * y), args)
+        elif shape == "circle":
+            perimeter = 2 * math.pi * args[0]
+        else:
+            print "The shape, %s, is not supported" % (shape)
+        return perimeter
+        
 
     @classmethod
     def area(shape, *args):
